1a9857dfca68520ce07a5295ab7f962a6923cc86
ycy <2423398379@qq.com>
2024-11-02 17:47:38 +0800

    complete selection sort method

--- lectureCode/3.1/testing/Sort.java
+++ lectureCode/3.1/testing/Sort.java
@@ -1,5 +1,21 @@
 public class Sort {
+    /** Use selection sort. */
     public static void sort(String[] x){
-
+        // Find smallest item.
+        // Move to the front.
+        // Selection sort the rest.
+        for(int i = 0;i<x.length;i++){
+            int indexOfMin = i;
+            for(int j = i+1;j<x.length;j++){
+                char min = x[indexOfMin].charAt(0);
+                char cur = x[j].charAt(0);
+                if(min>cur){
+                    indexOfMin = j;
+                }
+            }
+            String tmp = x[i];
+            x[i] = x[indexOfMin];
+            x[indexOfMin] = tmp;
+        }
     }
 }
