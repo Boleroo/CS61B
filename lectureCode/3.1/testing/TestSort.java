abd8fdb863998888615f2a4c72e0a22ee6f11601
ycy <2423398379@qq.com>
2024-11-03 16:11:32 +0800

    update TestSort

--- lectureCode/3.1/testing/TestSort.java
+++ lectureCode/3.1/testing/TestSort.java
@@ -7,7 +7,29 @@ public class TestSort {
         org.junit.Assert.assertArrayEquals(expected, input);
 
     }
+
+    /** Test the Sort.findSmallest method. */
+    public static void testFindSmallest(){
+        String[] input = {"i", "have", "an", "egg"};
+
+        int actual = Sort.findSmallest(input,0);
+        int expected = 2;
+        org.junit.Assert.assertEquals(expected, actual);
+
+        int actual2 = Sort.findSmallest(input,2);
+        int expected2 = 2;
+        org.junit.Assert.assertEquals(expected2, actual2);
+    }
+
+    /** Test the Sort.swap method. */
+    public static void testSwap(){
+        String[] input = {"i", "have", "an", "egg"};
+        Sort.swap(input, 1,2);
+        String[] expected = {"i", "an", "have", "egg"};
+        org.junit.Assert.assertArrayEquals(expected, input);
+    }
     public static void main(String[] args) {
+        //testFindSmallest();
         testSort();
     }
 }
