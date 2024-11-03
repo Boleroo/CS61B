48965409b9e33c89fd813c7b4b1e2b5c016dbbde
ycy <2423398379@qq.com>
2024-11-03 16:12:17 +0800

    Complete sort using recursive

--- lectureCode/3.1/testing/Sort.java
+++ lectureCode/3.1/testing/Sort.java
@@ -1,9 +1,6 @@
 public class Sort {
-    /** Use selection sort. */
-    public static void sort(String[] x){
-        // Find smallest item.
-        // Move to the front.
-        // Selection sort the rest.
+    /** Use selection sort(1st version). */
+    public static void sort1(String[] x){
         for(int i = 0;i<x.length;i++){
             int indexOfMin = i;
             for(int j = i+1;j<x.length;j++){
@@ -17,5 +14,42 @@ public class Sort {
             x[i] = x[indexOfMin];
             x[indexOfMin] = tmp;
         }
+    }
+
+    public static void sort(String[] x){
+        // Find smallest item.
+        // Move to the front.
+        // Selection sort the rest.
+        sort(0,x);
+
+    }
+
+    /** Sorts x starting at position start */
+    private static void sort(int startIndex, String[] x){
+        if(startIndex>x.length-1){
+            return;
+        }
+        int smallestIndex = findSmallest(x, startIndex);
+        swap(x, startIndex, smallestIndex);
+        sort(startIndex+1, x);
+    }
+
+    /** Return index of the smallest String in x. */
+    public static int findSmallest(String[] x, int start){
+        int smallestIndex = start;
+        for(int i = start;i<x.length;i++){
+            int cmp = x[i].compareTo(x[smallestIndex]);
+            if(cmp<0){
+                smallestIndex = i;
+            }
+        }
+        return smallestIndex;
+    }
+
+    /** Swap item a with b. */
+    public static void swap(String[] x, int a, int b){
+        String tmp = x[a];
+        x[a] = x[b];
+        x[b] = tmp;
     }
 }
