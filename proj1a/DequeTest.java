ef754f5c0fefa32a2e441dba1a44395c1e94e8b4
ycy <2423398379@qq.com>
2024-11-06 17:52:36 +0800

    modify test code

--- proj1a/DequeTest.java
+++ proj1a/DequeTest.java
@@ -5,6 +5,7 @@ import java.util.Deque;
 import static org.junit.Assert.*;
 public class DequeTest {
     public static void main(String[] args) {
+        /** Test LinkedListDeque. */
         LinkedListDeque<Integer> dq = new LinkedListDeque<>();
         dq.addFirst(1);
         dq.removeLast();
@@ -20,5 +21,17 @@ public class DequeTest {
         int res1 = dq.getRecursive(3);
         System.out.println(res1);
         dq.printDeque();
+
+        /** Test ArrayDeque. */
+        ArrayDeque<Integer> array = new ArrayDeque<>();
+        array.addFirst(1);
+        array.addFirst(2);
+        array.addFirst(3);
+        array.addFirst(4);
+        array.addFirst(5);
+        array.addLast(0);
+        array.addLast(-1);
+        array.addLast(-2);
+        array.addLast(-3);
     }
 }
