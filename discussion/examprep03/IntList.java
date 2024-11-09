d9eaeec43c9ff2324fa63ed6c51dd8aecbac7f10
ycy <2423398379@qq.com>
2024-11-09 22:04:20 +0800

    complete examprep03-3

--- discussion/examprep03/IntList.java
+++ discussion/examprep03/IntList.java
@@ -25,17 +25,33 @@ public class IntList {
         }
     }
 
+    public static void removeDuplicates(IntList p){
+        if(p == null){
+            return;
+        }
+        IntList current = p.rest;
+        IntList previous = p;
+        while(current != null) {
+            if(current.first == previous.first) {
+                previous.rest = current.rest;
+            } else {
+                previous = previous.rest;
+            }
+            current = previous.rest;
+        }
+    }
+
     public static void main(String[] args) {
-        IntList A = new IntList(10, null);
-        A = new IntList(9, A);
-        A = new IntList(8, A);
-        A = new IntList(7, A);
-        A = new IntList(6, A);
-        A = new IntList(5, A);
+        IntList A = new IntList(4, null);
+        A = new IntList(4, A);
         A = new IntList(4, A);
         A = new IntList(3, A);
+        A = new IntList(3, A);
+        A = new IntList(2, A);
+        A = new IntList(2, A);
         A = new IntList(2, A);
         A = new IntList(1, A);
-        A.skippify();
+        A = new IntList(1, A);
+        removeDuplicates(A);
     }
 }
