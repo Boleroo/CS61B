9b7cb96d769290153ba695677fc7e5fad2b0dbbf
ycy <2423398379@qq.com>
2024-11-10 20:57:56 +0800

    complete lecture code

---
+++ lectureCode/4.1/IsADemo.java
@@ -0,0 +1,9 @@
+public class IsADemo {
+    public static void main(String[] args) {
+        List61B<String> list = new SLList<>();
+        list.addFirst("crises");
+        list.addFirst("on");
+        list.addFirst("off");
+        list.print();
+    }
+}
---
+++ lectureCode/4.1/List61B.java
@@ -0,0 +1,18 @@
+public interface List61B<Item> {
+    public void addLast(Item x);
+    public Item getLast();
+    public Item get(int i);
+    public int size();
+    public Item removeLast();
+    public void insert(Item x, int position);
+    public void addFirst(Item x);
+    public Item getFirst();
+
+    default public void print() {
+        for (int i = 0; i < size(); i++) {
+            Item x = get(i);
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+}
