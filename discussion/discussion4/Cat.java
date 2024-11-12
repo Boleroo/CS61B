d61d47ad301929166019631388585253e4f8aabf
ycy <2423398379@qq.com>
2024-11-12 20:50:21 +0800

    Complete discussion4-1

--- discussion/discussion4/Cat.java
+++ discussion/discussion4/Cat.java
@@ -1,2 +1,18 @@
-public class Cat {
+public class Cat extends Animal {
+    /** Change noise in the constructor. */
+    public Cat(String name, int age){
+        super(name, age);
+        noise = "Meow!";
+    }
+
+    @Override
+    /** Override the greet method. */
+    public void greet() {
+        System.out.println("Cat " + name + "says: " + makeNoise());
+    }
+
+    public static void main(String[] args) {
+        Cat cat = new Cat("Cat", 1);
+        cat.greet();
+    }
 }
