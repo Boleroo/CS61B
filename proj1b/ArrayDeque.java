58b610c0b937944ad2b8cbe974ea3793817b7cec
ycy <2423398379@qq.com>
2024-11-14 15:52:12 +0800

    Complete task1

---
+++ proj1b/ArrayDeque.java
@@ -0,0 +1,119 @@
+public class ArrayDeque<T> implements Deque<T> {
+    private T[] array;
+    private int size;
+    private int nextFirst;
+    private int nextLast;
+    private final int usglen = 16;
+    /** Create an empty array deque. */
+    public ArrayDeque() {
+        array = (T[]) new Object[8];
+        size = 0;
+        nextFirst = 0;
+        nextLast = 1;
+    }
+
+    /** Resize method. */
+    private void resize(int newLen) {
+        T[] newArray = (T[]) new Object[newLen];
+        int cur = (nextFirst + 1) % array.length;
+        for (int i = 0; i < size; i++) {
+            newArray[i] = array[cur];
+            cur = (cur + 1) % array.length;
+        }
+        array = newArray;
+        nextFirst = newArray.length - 1;
+        nextLast = size;
+    }
+
+    /** Add item to head of the array. */
+    @Override
+    public void addFirst(T item) {
+        if (size == array.length) {
+            resize(array.length * 2);
+        }
+        array[nextFirst] = item;
+        nextFirst = (array.length + nextFirst - 1) % array.length;
+        size++;
+    }
+
+    /** Add item to back of the array. */
+    @Override
+    public void addLast(T item) {
+        if (size == array.length) {
+            resize(array.length * 2);
+        }
+        array[nextLast] = item;
+        nextLast = (nextLast + 1) % array.length;
+        size++;
+    }
+
+    /** Check if is empty. */
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /** Return number of items. */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /** Print items in the deque. */
+    @Override
+    public void printDeque() {
+        int len = array.length;
+        int cur = nextFirst;
+        for (int i = 1; i <= size; i++) {
+            cur = (cur + 1) % len;
+            System.out.println(array[cur] + " ");
+        }
+    }
+
+    /** Removes and returns the 1st item. */
+    @Override
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        int len = array.length;
+        int removeIndex = (nextFirst + 1) % len;
+        T res = array[removeIndex];
+        array[removeIndex] = null;
+        nextFirst = (nextFirst + 1) % len;
+        size--;
+        double usage = size / (double) len;
+        if (len >= usglen && usage < 0.25) {
+            resize(len / 2);
+        }
+        return res;
+    }
+
+    /** Removes and returns the last item. */
+    @Override
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        int len = array.length;
+        int removeIndex = (nextLast - 1 + len) % len;
+        T res = array[removeIndex];
+        array[removeIndex] = null;
+        nextLast = (nextLast - 1 + len) % len;
+        size--;
+        double usage = size / (double) len;
+        if (len >= usglen && usage < 0.25) {
+            resize(len / 2);
+        }
+        return res;
+    }
+
+    /** Return ith item, starts from 0. */
+    @Override
+    public T get(int index) {
+        if (index >= size) {
+            return null;
+        }
+        return array[(nextFirst + index + 1) % array.length];
+    }
+}
---
+++ proj1b/LinkedListDeque.java
@@ -0,0 +1,144 @@
+public class LinkedListDeque<T> implements Deque<T> {
+    private class Node<T> {
+        T item;
+        Node<T> next;
+        Node<T> prev;
+
+        private Node(T item) {
+            this.item = item;
+        }
+    }
+
+    private int size;
+    private Node<T> sentinel;
+
+    /** Creates an empty deque. */
+    public LinkedListDeque() {
+        sentinel = null;
+        size = 0;
+    }
+    /** Adds an item T to the front of the queue. */
+    @Override
+    public void addFirst(T item) {
+        if (size == 0) {
+            sentinel = new Node<>(item);
+            Node<T> first = new Node<>(item);
+            first.next = sentinel;
+            first.prev = sentinel;
+            sentinel.next = first;
+            sentinel.prev = first;
+        } else {
+            Node<T> newNode = new Node<>(item);
+            newNode.next = sentinel.next;
+            newNode.prev = sentinel;
+            sentinel.next.prev = newNode;
+            sentinel.next = newNode;
+        }
+        size++;
+    }
+
+    /** Adds an item T to the back of the queue. */
+    @Override
+    public void addLast(T item) {
+        if (size == 0) {
+            sentinel = new Node<>(item);
+            Node<T> first = new Node<>(item);
+            first.next = sentinel;
+            first.prev = sentinel;
+            sentinel.next = first;
+            sentinel.prev = first;
+        } else {
+            Node<T> newNode = new Node<>(item);
+            newNode.next = sentinel;
+            newNode.prev = sentinel.prev;
+            sentinel.prev.next = newNode;
+            sentinel.prev = newNode;
+        }
+        size++;
+    }
+
+    /** Check if is empty. */
+    @Override
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /** Get size of the list. */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /** Print items, separated by a space. */
+    @Override
+    public void printDeque() {
+        Node<T> cur = sentinel.next;
+        for (int i = 1; i <= size; i++) {
+            System.out.print(cur.item + " ");
+            cur = cur.next;
+        }
+    }
+
+    /** Removes and returns the item at the front of the queue. */
+    @Override
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        T res = sentinel.next.item;
+        Node<T> newHead = sentinel.next.next;
+        Node<T> removed = sentinel.next;
+        removed.next = null;
+        removed.prev = null;
+        sentinel.next = newHead;
+        newHead.prev = sentinel;
+        size--;
+        return res;
+    }
+
+    /** Removes and returns the last item. */
+    @Override
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        T res = sentinel.prev.item;
+        Node<T> removed = sentinel.prev;
+        Node<T> newRear = sentinel.prev.prev;
+        removed.prev = null;
+        removed.next = null;
+        sentinel.prev = newRear;
+        newRear.next = sentinel;
+        size--;
+        return res;
+    }
+
+    /** Gets ith item, starts from index 0. */
+    @Override
+    public T get(int index) {
+        if (size == 0) {
+            return null;
+        }
+        Node<T> cur = sentinel.next;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        return cur.item;
+    }
+
+    /** Get ith item, using recursion. */
+    public T getRecursive(int index) {
+        return getRecursive(sentinel.next, index);
+    }
+
+    /** A helper method. */
+    private T getRecursive(Node<T> node, int index) {
+        if (index == 0) {
+            return node.item;
+        }
+        return getRecursive(node.next, index - 1);
+    }
+}
