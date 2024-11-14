459a12b9cb37361476ed6f3b59ac3155a48c6dbf
ycy <2423398379@qq.com>
2024-11-14 22:01:17 +0800

    Complete task2

---
+++ proj1b/Palindrome.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+public class Palindrome {
+    /** Turn String into Deque. */
+    public Deque<Character> wordToDeque(String word) {
+        Deque<Character> dq = new LinkedListDeque<>();
+        for(int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            dq.addLast(c);
+        }
+        return dq;
+    }
+}
