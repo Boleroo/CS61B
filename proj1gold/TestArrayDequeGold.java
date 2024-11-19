ca2d6642075bb02086e0e2ba58649bf807a02c6c
ycy <2423398379@qq.com>
2024-11-19 22:17:19 +0800

    complete proj1gold

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,41 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestArrayDequeGold {
+    @Test
+    public void randomizedTest() {
+        StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
+        String msg = "";
+        while (true) {
+            double seed = StdRandom.uniform();
+            Integer num = (int) (10 * seed);
+            if (seed <= 0.25) {
+                sad.addFirst(num);
+                ads.addFirst(num);
+                Integer actual = sad.get(0);
+                Integer expected = ads.get(0);
+                msg = msg + "addFirst(" + num + ")\n";
+                assertEquals(msg, expected, actual);
+            } else if (seed > 0.25 && seed < 0.5) {
+                sad.addLast(num);
+                ads.addLast(num);
+                int index = ads.size() - 1;
+                Integer actual = sad.get(index);
+                Integer expected = ads.get(index);
+                msg = msg + "addLast(" + num + ")\n";
+                assertEquals(msg, expected, actual);
+            } else if (seed >= 0.5 && seed <= 0.75 && !ads.isEmpty()) {
+                Integer actual = sad.removeFirst();
+                Integer expected = ads.removeFirst();
+                msg = msg + "removeFirst()\n";
+                assertEquals(msg, expected, actual);
+            } else if (seed > 0.75 && !ads.isEmpty()) {
+                Integer actual = sad.removeLast();
+                Integer expected = ads.removeLast();
+                msg = msg + "removeLast()\n";
+                assertEquals(msg, expected, actual);
+            }
+        }
+    }
+}
